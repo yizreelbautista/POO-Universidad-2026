26dd21f8e4d6b08179c771c02c682a4abde84081
Yizreel Alexander Bautista Martinez <a251208@ugm.com.mx>
2026-06-06 10:14:42 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,62 @@
+package sistema.persistencia;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("===== EMPLEADO =====");
-        Empleado emp = new Empleado("Ana Torres", 30, "E-001", 20000.0);
-        emp.mostrarInformacion();
-        System.out.println("Nomina Neta Estimada: $" + emp.calcularSalarioNeto());
+        final String ARCHIVO_DATOS = "datos_estudiantes.dat";
+
+        System.out.println("=== SISTEMA DE PERSISTENCIA DIGITAL DE OBJETOS V1.0 ===\n");
+
+        List<Estudiante> registrosAcademicos = new ArrayList<>();
+
+        registrosAcademicos.add(
+                new Estudiante("S2101023", "Marco Mendoza", 9.5));
+
+        registrosAcademicos.add(
+                new Estudiante("S2101024", "Ana Mendoza", 9.8));
+
+        registrosAcademicos.add(
+                new Estudiante("S2101025", "Luis Mendoza", 8.9));
+
+        System.out.println(
+                "--- 1. Estado Inicial en la Memoria RAM Volátil ---");
+
+        registrosAcademicos.forEach(System.out::println);
 
         System.out.println();
-        System.out.println("===== GERENTE =====");
-        Gerente ger = new Gerente("Luis Perez", 45, "G-010", 40000.0,
-                                  "Operaciones", 8000.0);
-        ger.mostrarInformacion();
-        System.out.println("Nomina Neta Estimada: $" + ger.calcularSalarioNeto());
+
+        System.out.println(
+                "--- 2. Iniciando Flujo de Datos hacia Almacenamiento Secundario ---");
+
+        GestorPersistencia.guardarObjetos(
+                ARCHIVO_DATOS,
+                registrosAcademicos);
+
+        System.out.println();
+
+        registrosAcademicos = null;
+
+        System.out.println(
+                "--- 3. Simulación de Apagado: Datos de la RAM eliminados (Lista = null) ---\n");
+
+        System.out.println(
+                "--- 4. Leyendo Flujo de Entrada desde el Archivo Binario ---");
+
+        List<Estudiante> datosRecuperados =
+                GestorPersistencia.recuperarObjetos(ARCHIVO_DATOS);
+
+        System.out.println();
+
+        System.out.println(
+                "--- 5. Datos Rehidratados Exitosamente en Memoria RAM ---");
+
+        datosRecuperados.forEach(System.out::println);
+
+        System.out.println(
+                "\nConclusión: Los objetos sobrevivieron al ciclo de vida del programa.");
     }
 }
